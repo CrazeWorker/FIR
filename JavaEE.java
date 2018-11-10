bb1c083b040166ee8e67176b5dd9bc95399ed7d5
CrazeWorker <43256115+CrazeWorker@users.noreply.github.com>
2018-11-10 11:15:13 +0800

    JavaEE
    JavaEE

---
+++ JavaEE.java
@@ -0,0 +1,11 @@
+package www.Oracle_Day11_Test04;
+
+public class JavaEE extends WrEmployee{
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		System.out.println(this.name+"正在开发淘宝网站");
+	}
+
+}
