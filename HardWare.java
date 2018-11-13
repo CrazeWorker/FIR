8a307b452bcdfd3032f4021b4b3ada565a61a139
CrazeWorker <43256115+CrazeWorker@users.noreply.github.com>
2018-11-13 22:58:13 +0800

    HardWare
    HardWare

---
+++ HardWare.java
@@ -0,0 +1,9 @@
+package www.Oracle_Day12_Test;
+
+public class HardWare extends Maintainer {
+
+	public void work() {
+		System.out.println("员工号为"+super.getNumber()+"的硬件维护工程师"+super.getName()+"正在维修打印机");
+	}
+
+}
