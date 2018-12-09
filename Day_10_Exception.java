41c8204ddc75a8a736af8c4f252f1aa26e3eaa66
CrazeWorker <43256115+CrazeWorker@users.noreply.github.com>
2018-12-09 23:02:44 +0800

    Day_10_Exception
    Day_10_Exception

---
+++ Day_10_Exception.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Day_10_Exception {
+	public static void main(String[] args) {
+		
+//		// 算术异常
+//		int i = 1/0;
+//		// 空指针异常
+//		String s = null;
+//		s.equals("ccc");
+		new Scanner(System.in).nextInt();
+		}
+}
