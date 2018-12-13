2b2c1e5748cda3d221bdc5ac6c467f1719ad2978
CrazeWorker <43256115+CrazeWorker@users.noreply.github.com>
2018-12-13 22:55:26 +0800

    Burger
    Burger

---
+++ VegBurger.java
@@ -0,0 +1,17 @@
+package 建造者模式;
+
+public class VegBurger extends Burger{
+
+	@Override
+	public String name() {
+		// TODO Auto-generated method stub
+		return "VegBurger";
+	}
+
+	@Override
+	public double price() {
+		// TODO Auto-generated method stub
+		return 8.0;
+	}
+	
+}
