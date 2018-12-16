7610ddd0da6606e4a3d1e61c0089f2d1d1de6cf1
CrazeWorker <43256115+CrazeWorker@users.noreply.github.com>
2018-12-16 22:37:39 +0800

    Employee
    Employee

---
+++ Faculty.java
@@ -0,0 +1,18 @@
+package 第三天作业;
+
+public class Faculty extends Employee{
+	public String getDegree() {
+		return degree;
+	}
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+	public String getLevel() {
+		return level;
+	}
+	public void setLevel(String level) {
+		this.level = level;
+	}
+	private String degree;
+	private String level;
+}
