6f77c907317e4a2ef726833c5882cbbdbe7cec78
CrazeWorker <43256115+CrazeWorker@users.noreply.github.com>
2018-12-29 23:35:00 +0800

    MethodDemo
    MethodDemo

---
+++ MethodDemo.java
@@ -0,0 +1,9 @@
+public class MethodDemo{
+	public static void main(String [] args){
+		CountArea(1,3);
+	}
+	public static double CountArea(int length,int width){
+		int area = length * width;
+		return area;
+	}
+}
